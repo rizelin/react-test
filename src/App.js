fec8b128597b6ce7a02b8ff0823fd3cfd901e830
baekbfp <baekbfp@gmail.com>
2024-02-18 21:01:03 +0900

    색 2-16

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React,{ useState } from 'react'; 
 import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 function App() {
   const [title, setTitle] = useState('');
@@ -13,38 +19,50 @@ function App() {
   }
 
   return (
-    <React.StrictMode>
-      <div className="container">
-        <div className="mb-3">
-          <label className="form-label">Title</label>
-          <input
-            className="form-control"
-            value={title}
-            onChange={(evnet) => {
-              setTitle(evnet.target.value )
-            }}  
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Body</label>
-          <textarea
-            className="form-control"
-            value={body}
-            onChange={(evnet) => {
-              setBody(evnet.target.value )
-            }}  
-          />
-        </div>
-        <div>
-          <button
-            className="btn btn-primary"
-            onClick={onSubmit}  
-          >
-            Post
-          </button>
-        </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/blogs">Blogs</Link>
       </div>
-    </React.StrictMode>
+      <Switch>
+        <Route path="/" exact>
+          Home Page
+        </Route>
+        <Route path="/blogs" exact>
+          <div className="container">
+            <div className="mb-3">
+              <label className="form-label">Title</label>
+              <input
+                className="form-control"
+                value={title}
+                onChange={(evnet) => {
+                  setTitle(evnet.target.value )
+                }}  
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label">Body</label>
+              <textarea
+                className="form-control"
+                value={body}
+                onChange={(evnet) => {
+                  setBody(evnet.target.value )
+                }}  
+              />
+            </div>
+            <div>
+              <button
+                className="btn btn-primary"
+                onClick={onSubmit}  
+              >
+                Post
+              </button>
+            </div>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+
   )
 }
 export default App;
