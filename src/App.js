71a52afcedeaa78f0ffec4128c20783a65ebc4a1
baekbfp <baekbfp@gmail.com>
2024-02-18 14:46:35 +0900

    색2-10

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React,{ useState } from 'react'; 
 
 function App() {
-  console.log('lender')
-  const [number, setNumber] = useState(1)
-  const dobule = () => {
-    setNumber((prevState) => prevState * 2 ); 
-    setNumber((prevState) => prevState * 2 ); 
-  };
-
-  // const pringHello = () => {
-  //   console.log('Hello')
-  // }
-  // 색션2-10
+  const [title, setTitle] = useState('');
 
   return (
     <React.StrictMode>
-    <div>
-      {number} 
-    </div>
-    <button className="btn btn-primary" onClick={dobule}>submit</button>
+      <div className="container">
+        <div className="mb-3">
+          <label className="form-label">Title</label>
+          <input
+            className="form-control"
+            value={title}
+            onChange={(evnet) => {
+              setTitle(evnet.target.value )
+            }}  
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">
+            Post
+          </button>
+        </div>
+      </div>
     </React.StrictMode>
   )
 }
-
 export default App;
